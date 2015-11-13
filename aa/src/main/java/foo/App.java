264762428488404ff16185508b843fbefc1afbee
dawnsnow <zhao_7070@163.com>
2015-11-13 11:00:42 +0800

    new

--- aa/src/main/java/foo/App.java
+++ aa/src/main/java/foo/App.java
@@ -8,6 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	int aac = 0;
+        System.out.println( "Hello World!"  + aac);
     }
 }
